1d91cbf65e0bc5a0b7fca3ee080a9fcf39edd4f1
JeyxeL <jhonatanlapo2003@gmail.com>
2025-04-29 19:13:49 -0500

    Nuevo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const res = await fetch('https://api.themoviedb.org/3/tv/popular?api_key=' + API_KEY);
     const data = await res.json();
   
     const movies = data.results;
